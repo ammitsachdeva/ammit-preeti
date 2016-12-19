dc1dbce1f46e0f06a61a14197d5a47772ad7667c
preetti <preetisachdeva@gmx.de>
2016-12-19 15:46:16 +0100

    unit test add products done

--- WorkingWithVisualStudio/src/WorkingWithVisualStudio/Controllers/HomeController.cs
+++ WorkingWithVisualStudio/src/WorkingWithVisualStudio/Controllers/HomeController.cs
@@ -9,8 +9,16 @@ namespace WorkingWithVisualStudio.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
-            => View(SimpleRepository.SharedRepository.Products
-                .Where(p => p.Price < 50));
+     SimpleRepository Repository = SimpleRepository.SharedRepository;
+    public IActionResult Index() => View(Repository.Products
+                               .Where(p => p?.Price < 50));
+    [HttpGet]
+    public IActionResult AddProduct() => View(new Product());
+    [HttpPost]
+    public IActionResult AddProduct(Product p)
+    {
+        Repository.AddProduct(p);
+        return RedirectToAction("Index");
     }
+}
 }
